4a3459d58aff78e0ccfd7247920feb2a06d30869
Jon Lange <jlange@microsoft.com>
2024-01-20 22:33:35 -0800

    Define nesting levels for GHCB access

--- src/cpu/ghcb.rs
+++ src/cpu/ghcb.rs
@@ -10,15 +10,26 @@ use crate::sev::ghcb::GHCB;
 use core::ops::{Deref, DerefMut};
 use core::ptr;
 
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub enum GHCBNestingLevel {
+    Normal = 0,
+    Console = 1,
+    Debugger = 2,
+}
+
 #[derive(Debug)]
 pub struct GHCBState {
     ghcb: *mut GHCB,
+    nesting_level: GHCBNestingLevel,
+    borrowed: bool,
 }
 
 impl GHCBState {
     pub fn new() -> GHCBState {
         GHCBState {
             ghcb: ptr::null_mut(),
+            nesting_level: GHCBNestingLevel::Normal,
+            borrowed: false,
         }
     }
 
@@ -34,11 +45,13 @@ impl GHCBState {
 #[derive(Debug)]
 pub struct GHCBRef {
     ghcb_state: *mut GHCBState,
+    previous_level: GHCBNestingLevel,
+    was_borrowed: bool,
 }
 
 impl Deref for GHCBRef {
     type Target = GHCB;
-    fn deref(&self) -> &'static GHCB {
+    fn deref(&self) -> &GHCB {
         unsafe {
             let ghcb_state = &*self.ghcb_state;
             &*ghcb_state.ghcb
@@ -47,7 +60,7 @@ impl Deref for GHCBRef {
 }
 
 impl DerefMut for GHCBRef {
-    fn deref_mut(&mut self) -> &'static mut GHCB {
+    fn deref_mut(&mut self) -> &mut GHCB {
         unsafe {
             let ghcb_state = &*self.ghcb_state;
             &mut *ghcb_state.ghcb
@@ -55,12 +68,40 @@ impl DerefMut for GHCBRef {
     }
 }
 
+impl Drop for GHCBRef {
+    fn drop(&mut self) {
+        unsafe {
+            let ghcb_state = &mut *self.ghcb_state;
+            assert!(ghcb_state.borrowed);
+            ghcb_state.borrowed = self.was_borrowed;
+            ghcb_state.nesting_level = self.previous_level;
+        }
+    }
+}
+
 pub fn current_ghcb() -> GHCBRef {
-    // FIXME - Add borrow checking to GHCB references.
+    nested_ghcb(GHCBNestingLevel::Normal)
+}
+
+pub fn nested_ghcb(level: GHCBNestingLevel) -> GHCBRef {
     unsafe {
         let cpu_ptr = this_cpu_unsafe();
         let cpu = &mut *cpu_ptr;
         let ghcb_state = cpu.ghcb_state();
-        GHCBRef { ghcb_state }
+        let previous_level = ghcb_state.nesting_level;
+
+        // Recursive borrowing is allowable if the the new nesting level is
+        // strictly higher than the old level.
+        let was_borrowed = ghcb_state.borrowed;
+        if was_borrowed && level <= previous_level {
+            panic!("GHCB borrowed recursively");
+        }
+        ghcb_state.borrowed = true;
+        ghcb_state.nesting_level = level;
+        GHCBRef {
+            ghcb_state,
+            previous_level,
+            was_borrowed,
+        }
     }
 }
