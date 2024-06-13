4100f7cd21f088b5bfaf5f54a810ad59f7013875
Jon Lange <jlange@microsoft.com>
2024-06-13 16:19:40 -0700

    cpu: Support TPR when handling SVSM interrupts
    The priority scheme inherently associated with interrupts permits
    masking interrupts of lower priority classes while handling
    higher-priority interrupts. This makes it possible to enable interrupts
    while in an interrupt handler, allowing high-priority interrupts to be
    handled while masking lower-priorty interrupts.  By raising TPR during
    interrupt handling, it becomes possible to be safely preempted by
    higher-priority interrupts during interrupt handling.
    
    This change also defines a mechanism where code unrelated to interrupt
    handlers can temporarily raise TPR to mask low-priority interrupts while
    permitting the delivery of high-priority interrupts.  In the future, it
    will be possible to enable spin locks or other locks to be associated
    with TPR so that a lock can safely be acquired and prevent reentracncy
    by low-priority interrupts while still permitting the handilng of
    high-priority interrupts.
    
    Signed-off-by: Jon Lange <jlange@microsoft.com>

--- kernel/src/cpu/percpu.rs
+++ kernel/src/cpu/percpu.rs
@@ -460,6 +460,28 @@ impl PerCpu {
         self.irq_state.count()
     }
 
+    /// Raises TPR on the current CPU.  Keeps track of the nesting level.
+    ///
+    /// The caller must ensure that every `raise_tpr()` call is followed by a
+    /// matching call to `lower_tpr()`.
+    #[inline(always)]
+    pub fn raise_tpr(&self, tpr_value: usize) {
+        self.irq_state.raise_tpr(tpr_value);
+    }
+
+    /// Lowers TPR from the current level to the new level required by the
+    /// current nesting state.
+    ///
+    /// The caller must ensure that a `lower_tpr()` call balances a preceding
+    /// `raise_tpr()` call to the indicated level.
+    ///
+    /// * `tpr_value` - The TPR from which the caller would like to lower.
+    ///   Must be less than or equal to the current TPR.
+    #[inline(always)]
+    pub fn lower_tpr(&self, tpr_value: usize) {
+        self.irq_state.lower_tpr(tpr_value);
+    }
+
     /// Sets up the CPU-local GHCB page.
     pub fn setup_ghcb(&self) -> Result<(), SvsmError> {
         let page = GhcbPage::new()?;
@@ -1105,6 +1127,28 @@ pub fn irqs_enable() {
 /// Current nesting depth of irq_disable() calls.
 pub fn irq_nesting_count() -> isize {
     this_cpu().irq_nesting_count()
+}
+
+/// Raises TPR on the current CPU.  Keeps track of the nesting level.
+///
+/// The caller must ensure that every `raise_tpr()` call is followed by a
+/// matching call to `lower_tpr()`.
+#[inline(always)]
+pub fn raise_tpr(tpr_value: usize) {
+    this_cpu().raise_tpr(tpr_value);
+}
+
+/// Lowers TPR from the current level to the new level required by the
+/// current nesting state.
+///
+/// The caller must ensure that a `lower_tpr()` call balances a preceding
+/// `raise_tpr()` call to the indicated level.
+///
+/// * `tpr_value` - The TPR from which the caller would like to lower.
+///   Must be less than or equal to the current TPR.
+#[inline(always)]
+pub fn lower_tpr(tpr_value: usize) {
+    this_cpu().lower_tpr(tpr_value);
 }
 
 /// Gets the GHCB for this CPU.
