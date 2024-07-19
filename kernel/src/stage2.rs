fbc82af2837e0e18716ff363929bd519def6802a
Jon Lange <jlange@microsoft.com>
2024-07-19 15:32:37 -0700

    Establish VTOM during early platform initialization

--- kernel/src/stage2.rs
+++ kernel/src/stage2.rs
@@ -80,7 +80,7 @@ fn setup_env(
     let debug_serial_port = config.debug_serial_port();
     install_console_logger("Stage2").expect("Console logger already initialized");
     platform
-        .env_setup(debug_serial_port)
+        .env_setup(debug_serial_port, launch_info.vtom.try_into().unwrap())
         .expect("Early environment setup failed");
 
     // Validate the first 640 KB of memory so it can be used if necessary.
@@ -106,7 +106,7 @@ fn setup_env(
     };
 
     register_cpuid_table(cpuid_page);
-    paging_init_early(platform, launch_info.vtom).expect("Failed to initialize early paging");
+    paging_init_early(platform).expect("Failed to initialize early paging");
 
     set_init_pgtable(PageTableRef::shared(unsafe { addr_of_mut!(pgtable) }));
 
