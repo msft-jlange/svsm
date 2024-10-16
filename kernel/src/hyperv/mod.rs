f80e4029652331846f07a3e7c9871a7b52d3424c
Jon Lange <jlange@microsoft.com>
2024-10-16 15:54:24 -0700

    hyperv: introduce VP context structures

--- kernel/src/hyperv/mod.rs
+++ kernel/src/hyperv/mod.rs
@@ -9,3 +9,43 @@ pub mod msr;
 
 pub use hyperv::*;
 pub use msr::*;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default)]
+pub struct HvSegmentRegister {
+    pub base: u64,
+    pub limit: u32,
+    pub selector: u16,
+    pub attributes: u16,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default)]
+pub struct HvTableRegister {
+    pub _rsvd: [u16; 3],
+    pub limit: u16,
+    pub base: u64,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default)]
+pub struct HvInitialVpContext {
+    pub rip: u64,
+    pub rsp: u64,
+    pub rflags: u64,
+    pub cs: HvSegmentRegister,
+    pub ds: HvSegmentRegister,
+    pub es: HvSegmentRegister,
+    pub fs: HvSegmentRegister,
+    pub gs: HvSegmentRegister,
+    pub ss: HvSegmentRegister,
+    pub tr: HvSegmentRegister,
+    pub ldtr: HvSegmentRegister,
+    pub idtr: HvTableRegister,
+    pub gdtr: HvTableRegister,
+    pub efer: u64,
+    pub cr0: u64,
+    pub cr3: u64,
+    pub cr4: u64,
+    pub pat: u64,
+}
