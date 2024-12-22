abb4b797a8d46c7180ca3a2fcad1938ce0de2c89
Jon Lange <jlange@microsoft.com>
2024-12-22 11:14:38 -0800

    platform/native: require the use of X2APIC
    For simplicity and consistency, require the native platform to use
    X2APIC so that APIC management can be identical across all native
    environments.
    
    Signed-off-by: Jon Lange <jlange@microsoft.com>

--- kernel/src/platform/native.rs
+++ kernel/src/platform/native.rs
@@ -9,7 +9,7 @@ use crate::console::init_svsm_console;
 use crate::cpu::apic::{ApicIcr, IcrMessageType};
 use crate::cpu::control_regs::read_cr3;
 use crate::cpu::cpuid::CpuidResult;
-use crate::cpu::msr::write_msr;
+use crate::cpu::msr::{read_msr, write_msr};
 use crate::cpu::percpu::PerCpu;
 use crate::cpu::smp::create_ap_start_context;
 use crate::error::SvsmError;
@@ -29,6 +29,9 @@ use crate::mm::virt_to_phys;
 
 #[cfg(test)]
 use bootlib::platform::SvsmPlatformType;
+
+const MSR_APIC_BASE: u32 = 0x1B;
+const APIC_X2_ENABLE_MASK: u64 = 0xC00;
 
 const APIC_MSR_EOI: u32 = 0x80B;
 const APIC_MSR_ICR: u32 = 0x830;
@@ -50,6 +53,11 @@ impl NativePlatform {
 
 impl Default for NativePlatform {
     fn default() -> Self {
+        // Execution is not possible unless X2APIC is supported.
+        let features = CpuidResult::get(1, 0);
+        if (features.ecx & 0x200000) == 0 {
+            panic!("X2APIC is not supported");
+        }
         Self::new()
     }
 }
@@ -87,6 +95,15 @@ impl SvsmPlatform for NativePlatform {
     }
 
     fn setup_percpu_current(&self, _cpu: &PerCpu) -> Result<(), SvsmError> {
+        // Enable X2APIC mode.
+        // SAFETY: accesses to the APIC_BASE MSR are safe if the address is not
+        // being changed.
+        let apic_base = read_msr(MSR_APIC_BASE);
+        let apic_base_x2_enabled = apic_base | APIC_X2_ENABLE_MASK;
+        if apic_base != apic_base_x2_enabled {
+            write_msr(MSR_APIC_BASE, apic_base_x2_enabled);
+        }
+
         Ok(())
     }
 
