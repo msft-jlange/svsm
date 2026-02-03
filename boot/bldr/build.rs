238c1fa8502704d29090efdc05153a42072a7bd9
Jon Lange <jlange@microsoft.com>
2026-02-03 14:23:51 -0800

    new bldr

---
+++ boot/bldr/build.rs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2022-2023 SUSE LLC
+//
+// Author: Joerg Roedel <jroedel@suse.de>
+
+fn main() {
+    println!("cargo:rustc-link-arg=-nostdlib");
+    println!("cargo:rustc-link-arg=--build-id=none");
+    println!("cargo:rustc-link-arg=-Tboot/bldr/src/bldr.lds");
+    println!("cargo:rustc-link-arg=-no-pie");
+    println!("cargo:rerun-if-changed=boot/bldr/src/bldr.lds");
+    println!("cargo:rerun-if-changed=build.rs");
+}
